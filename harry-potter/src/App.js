4fe9ba30f63c6bb9ce1f42ff0db7f243e57c3147
CarolinaOM <caro13081991@gmail.com>
2024-10-09 17:08:31 +0100

    aplicando ternarios

--- harry-potter/src/App.js
+++ harry-potter/src/App.js
@@ -1,27 +1,30 @@
 import imageHarryPotter from './img/harry-potter.png';
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 import Films from './components/Films';
 
 function App() {
-  const [fimls, setFilms] = useState(null);
+  const [films, setFilms] = useState(null);
 
-  const reqApi = async() => {
-    const api = await fetch ("https://potterhead-api.vercel.app/api/movies")
+  const reqApi = async () => {
+    const api = await fetch("https://potterhead-api.vercel.app/api/movies");
     const filmApi = await api.json();
-    console.log(filmApi);
-    setFilms(filmApi.results);
+    console.log(filmApi); // Verifica que llegan los datos de la API
+    setFilms(filmApi); // Si la API devuelve un objeto completo y no solo "results"
   };
 
-  console.log(fimls);
-  
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Harry Potter</h1>
-        <Films></Films>
-        <img src={imageHarryPotter} alt="Harry Potter" className="img-home" />
-        <button onClick={reqApi} className="btn-search">Buscar Peliculas</button>
+        {films ? (
+          <Films films={films}></Films>
+        ) : (
+          <>
+            <img src={imageHarryPotter} alt="Harry Potter" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Peliculas</button>
+          </>
+        )}
       </header>
     </div>
   );
--- harry-potter/src/components/Films.js
+++ harry-potter/src/components/Films.js
@@ -1,4 +1,5 @@
-export default function Films(){
+export default function Films(props){
+    console.log(props);
    return (
     <div>
         <h1>Desde Filsm Js</h1>
